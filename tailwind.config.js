fca1ed5937aeeded211891c6eb86e9678889d962
Ife-Banks <ifeoluwa.bankole05@gmail.com>
2025-03-23 17:26:58 +0100

    responsiveness of login

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js (just for IntelliSense support)
+export default {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#1e40af',
+          secondary: '#f43f5e',
+        },
+      },
+    },
+  }
+  
